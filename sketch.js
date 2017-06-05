9ca4f1be1fc96676fae26c391f12a1bbb098f9f8
Jamie Quinn <j.quinn.1@research@gla.ac.uk>
2017-06-05 15:33:59 +0100

    Lorenz attractor now working fully with varying line thickness for depth illusion

--- sketch.js
+++ sketch.js
@@ -1,58 +1,83 @@
-var x = [];
-var y = [];
-var z = [];
-
 var SIGMA=10;
 var BETA=8/3;
 var RHO=28;
 
-var SCALE=7;
+var MAX_POINTS = 200;
 
-var WEBGL_ENABLED = false;
+var SCALE=7;
 
 var dt = 0.01;
 
-function setup() {
-  if (WEBGL_ENABLED) {
-    createCanvas(windowHeight, windowHeight, WEBGL);
-  } else {
-    createCanvas(windowHeight, windowHeight);
-  }
-  frameRate(60);
+var paths = []
 
-  x.push(0.1);
-  y.push(0);
-  z.push(0);
+function setup() {
+  createCanvas(windowHeight, windowHeight);
+  frameRate(60);
+}
+
+function mouseReleased() {
+  var x = (mouseX - width/2)/SCALE;
+  var y = (mouseY - width/2)/SCALE;
+  paths.push(new Path(x, y, 0));
 }
 
 function draw() {
   background(51);
-  var px = x[x.length-1];
-  var py = y[y.length-1];
-  var pz = z[z.length-1];
+  translate(width/2, height/2);
+  for (var i=0; i < paths.length; ++i) {
+    p = paths[i];
+    var px = p.x[p.last];
+    var py = p.y[p.last];
+    var pz = p.z[p.last];
 
-  var dx = SIGMA*(py-px)*dt;
-  var dy = (px*(RHO-pz) - py)*dt;
-  var dz = (px*py - BETA*pz)*dt;
+    var dx = SIGMA*(py-px);
+    var dy = px*(RHO-pz) - py;
+    var dz = px*py - BETA*pz;
 
-  x.push(px+dx);
-  y.push(py+dy);
-  z.push(pz+dz);
+    p.velocity.set(dx, dy, dz);
 
-  if (WEBGL_ENABLED) {
-  beginShape();
-  for (var j = 0; j < x.length; ++j) {
-    var val = j/x.length*204 + 51;
-    fill(val);
-    vertex(SCALE*x[j], SCALE*y[j], SCALE*z[j]);
+    p.update();
+    p.display();
   }
-  endShape();
-  } else {
-    translate(width/2, height/2);
-    for (var j = 1; j < x.length; ++j) {
-      var val = j/x.length*204 + 51;
-      stroke(val);
-      line(SCALE*x[j-1], SCALE*y[j-1], SCALE*x[j], SCALE*y[j]);
-    }
+}
 
-}}
+// A simple Path class
+var Path = function(x_0, y_0, z_0) {
+  this.x = Array.apply(null, Array(MAX_POINTS)).map(Number.prototype.valueOf,x_0);
+  this.y = Array.apply(null, Array(MAX_POINTS)).map(Number.prototype.valueOf,y_0);
+  this.z = Array.apply(null, Array(MAX_POINTS)).map(Number.prototype.valueOf,z_0);
+  this.last = 0;
+  this.velocity = createVector(0, 0, 0);
+};
+
+Path.prototype.run = function() {
+  this.update();
+  this.display();
+};
+
+// Method to update position
+Path.prototype.update = function(){
+  var prev = this.last;
+  this.last = (this.last+1) % MAX_POINTS;
+  this.x[this.last] = this.x[prev] + this.velocity.x * dt;
+  this.y[this.last] = this.y[prev] + this.velocity.y * dt;
+  this.z[this.last] = this.z[prev] + this.velocity.z * dt;
+};
+
+// Method to display
+Path.prototype.display = function() {
+  var count = 0;
+  var j = (this.last+1)%MAX_POINTS;
+  var pj = this.last;
+  while (count < MAX_POINTS - 1 ){
+    pj = j;
+    j = (j+1) % MAX_POINTS;
+    ++count;
+    var val = count/MAX_POINTS*204 + 51;
+    stroke(val);
+    var linewidth = map(this.z[j], 14, 30, 0.5, 2);
+    //strokeWeight(linewidth);
+    //line(SCALE*this.x[pj], SCALE*this.y[pj], SCALE*this.x[j], SCALE*this.y[j]);
+    ellipse(SCALE*this.x[j], SCALE*this.y[j], linewidth, linewidth);
+  }
+};
